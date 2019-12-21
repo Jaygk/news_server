c398c7b8ed3685fd0881fa893e9b2d21435ec220
Jaygk <957569035@qq.com>
2019-12-21 21:44:28 +0800

    :sparkles: 爬取推荐新闻列表

---
+++ src/request/recommend/index.js
@@ -0,0 +1,34 @@
+const request = require('../request')
+const News = require('../../store')
+
+// 爬取推荐新闻列表（标题+url）
+module.exports = async () => {
+  try {
+    const $ = await request('/')
+
+    const list = $('#syncad_1').find('a')
+
+    let arr = []
+    list.each((i, item) => {
+      let obj = {}
+      obj.title = $(item).text().trim()
+      obj.url = $(item).attr('href').split('.cn')[1]
+      arr.push(obj)
+    })
+
+    const news = new News({
+      name: 'recommend',
+      type: '推荐',
+      data: arr,
+    })
+
+    const temp = await News.find({ name: 'recommend' })
+    if (temp.length !== 0) {
+      await News.updateOne({ name: 'recommend' }, { data: arr })
+    } else {
+      await news.save()
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
